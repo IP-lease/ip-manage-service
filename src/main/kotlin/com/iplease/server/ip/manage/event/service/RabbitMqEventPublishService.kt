b04262bfcd5b2fee4c9e343187826b821c8a18c8
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-09 15:50:33 +0900

    [ADD] EventPublishing 로직 작성

---
+++ src/main/kotlin/com/iplease/server/ip/manage/event/service/RabbitMqEventPublishService.kt
@@ -0,0 +1,17 @@
+package com.iplease.server.ip.manage.event.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class RabbitMqEventPublishService(
+    val rabbitTemplate: RabbitTemplate,
+): EventPublishService {
+    companion object { const val EXCHANGE_NAME = "iplease.event" }
+    override fun <T: Any> publish(routingKey: String, data: T): T =
+        ObjectMapper().registerModule(KotlinModule()).writeValueAsString(data)
+            .let { rabbitTemplate.convertAndSend(EXCHANGE_NAME, routingKey, it) }
+            .let { data }
+}

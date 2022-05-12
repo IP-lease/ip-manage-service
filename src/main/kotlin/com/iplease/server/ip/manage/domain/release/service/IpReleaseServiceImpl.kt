6f5b5ca68a38dba80dfe20874b3068b672775428
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-12 10:26:15 +0900

    [FIX] 누락된 Service Annotation 작성

--- src/main/kotlin/com/iplease/server/ip/manage/domain/release/service/IpReleaseServiceImpl.kt
+++ src/main/kotlin/com/iplease/server/ip/manage/domain/release/service/IpReleaseServiceImpl.kt
@@ -2,9 +2,11 @@ package com.iplease.server.ip.manage.domain.release.service
 
 import com.iplease.server.ip.manage.domain.release.exception.UnknownAssignedIpException
 import com.iplease.server.ip.manage.global.common.repository.AssignedIpRepository
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 
+@Service
 class IpReleaseServiceImpl(
     private val ipReleaseRepository: AssignedIpRepository
 ): IpReleaseService {

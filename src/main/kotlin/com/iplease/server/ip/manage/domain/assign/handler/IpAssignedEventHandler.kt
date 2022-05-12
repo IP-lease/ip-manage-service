f369b5043cac43fa29cea6481e02c33c1e7ca518
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-12 09:04:37 +0900

    [FIX] IpAssignedEventHandler 수정 및 적용

--- src/main/kotlin/com/iplease/server/ip/manage/domain/assign/handler/IpAssignedEventHandler.kt
+++ src/main/kotlin/com/iplease/server/ip/manage/domain/assign/handler/IpAssignedEventHandler.kt
@@ -1,7 +1,18 @@
 package com.iplease.server.ip.manage.domain.assign.handler
 
 import com.iplease.server.ip.manage.global.common.data.dto.AssignedIpDto
+import reactor.core.publisher.Mono
 
 interface IpAssignedEventHandler {
-    fun handle(dto: AssignedIpDto)
+    fun handle(dto: AssignedIpDto) {
+        onStart(dto)
+            .doOnError{ onError(dto, it) }
+            .doOnSuccess { onComplete(dto, it) }
+            .onErrorReturn(dto)
+            .block()
+    }
+
+    fun onStart(dto: AssignedIpDto): Mono<AssignedIpDto>
+    fun onError(dto: AssignedIpDto, error: Throwable)
+    fun onComplete(request: AssignedIpDto, response: AssignedIpDto)
 }
